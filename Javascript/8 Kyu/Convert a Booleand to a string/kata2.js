91e3923bc9398df71fea195046267e5e4a5ca49b
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-09-11 16:55:10 -0500

    ADDED

---
+++ Javascript/8 Kyu/Convert a Booleand to a string/kata2.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+function booleanToString(b){
+  if(b){
+    return "true";
+  }else{
+   return "false";
+ }
+ // b.toString();
+///////////
+ // if ( b.toString() ){
+  //return "true";
+ //} else {
+   //return "false";
+// }
+//if b.toString() {
+  //return "true";
+//} else{
+ // return "false";
+//}
+}
